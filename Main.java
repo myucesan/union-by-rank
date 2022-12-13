ea8e0068704879b8a6a5b2fc653c51f74b65dbbd
myucesan <my96@live.nl>
2022-12-13 12:15:37 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
+
+static class UnionFind {
+  private int[] parent;
+  private int[] rank;
+
+  public UnionFind(int n) {
+    parent = new int[n];
+    rank = new int[n];
+
+    
+  }
+
+}
